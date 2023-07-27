110e88b5d401fe57fb55f939968aa0a883df578e
Carlos Enrique Hoy Chi <enrique@thiio.com>
2023-07-27 12:16:33 -0500

    folders renamed

---
+++ src/Tests/ClientTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Thiio\ShipOffers\Test;
+
+use PHPUnit\Framework\TestCase;
+use Thiio\ShipOffers\Client;
+use Thiio\ShipOffers\Exceptions\InvalidArgumentException;
+
+final class ClientTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function buildClient(): void
+    {
+        $client = new Client("myusername", "mypassword", 'asdasdsa');
+        $this->assertInstanceOf(Client::class, $client);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldThrowAnErrorDueLackOfUsernam(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Username is a required parameter');
+        $client = new Client(null, "mypassword", 'asdasdsa');
+    }
+
+     /**
+     * @test
+     */
+    public function itShouldThrowAnErrorDueLackOfPassword(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Password is a required parameter');
+        $client = new Client("myusername", null, 'asdasdsa');
+    }
+}
---
+++ src/Tests/OrderTest.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace Thiio\ShipOffers\Test;
+
+use PHPUnit\Framework\TestCase;
+use Thiio\ShipOffers\Handlers\OrderHandler;
+use Thiio\ShipOffers\Models\Order;
+
+final class OrderTest extends TestCase
+{
+    private const USERNAME = 'systems+api-caf001@shipoffers.com';
+    private const PASSWORD = 'r7nvYJOK7RjRsAAm';
+    private const STORE_ID = 'e8f83515-71d5-4500-8019-9a7b894afd73';
+
+    private $orderHandler;
+
+    /**
+     * PHPunit setUp method
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        $this->orderHandler = new OrderHandler(self::USERNAME, self::PASSWORD, self::STORE_ID);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldBuildOrderHandler() : void
+    {
+        $this->assertInstanceOf(OrderHandler::class, $this->orderHandler);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldThrowAnErrorIfOrderIdIsNotSet() : void
+    {
+        $response = $this->orderHandler->fetchOrder();
+        $this->assertEquals('Missing required parameter Order Id', $response->error);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldFetchAnOrder() : void
+    {
+        $orderNumber = date("YmdHis") . rand(0, 100);
+        $payload = [
+            "order_number" => $orderNumber,
+            "requested_shipping_service" => "UPS First Class",
+            "ship_name" => "TEST William Adama",
+            "address1" => "3534 Cole Prairie",
+            "address2" => "Suite 703",
+            "city" => "East Justine",
+            "state" => "North Carolina",
+            "postal_code" => "56957-3212",
+            "country" => "US",
+            "phone" => "1-514-133-0708",
+            "email" => null
+        ];
+
+        $order = new Order($payload);
+        $responseCreateOrder = $this->orderHandler->createOrder($order);
+        $this->assertEquals($orderNumber, $responseCreateOrder->order->getOrderNumber());
+
+        $responseFetchOrder = $this->orderHandler->fetchOrder($responseCreateOrder->order->getId());
+        $this->assertEquals($responseCreateOrder->order->getId(), $responseFetchOrder->order->getId());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldFetchOrders() : void
+    {
+        $response = $this->orderHandler->fetchOrders();
+        $this->assertContainsOnlyInstancesOf(Order::class, $response->orders);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldCreateAnOrder() : void
+    {
+        $orderNumber = date("YmdHis") . rand(0, 100);
+        $payload = [
+            "order_number" => $orderNumber,
+            "requested_shipping_service" => "UPS First Class",
+            "ship_name" => "TEST William Adama",
+            "address1" => "3534 Cole Prairie",
+            "address2" => "Suite 703",
+            "city" => "East Justine",
+            "state" => "North Carolina",
+            "postal_code" => "56957-3212",
+            "country" => "US",
+            "phone" => "1-514-133-0708",
+            "email" => null
+        ];
+
+        $order = new Order($payload);
+        $response = $this->orderHandler->createOrder($order);
+        $this->assertEquals($orderNumber, $response->order->getOrderNumber());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldUpdateAnOrder() : void
+    {
+        $orderNumber = date("YmdHis") . rand(0, 100);
+        $createPayload = [
+            "order_number" => $orderNumber,
+            "requested_shipping_service" => "UPS First Class",
+            "ship_name" => "TEST William Adama",
+            "address1" => "3534 Cole Prairie",
+            "address2" => "Suite 703",
+            "city" => "East Justine",
+            "state" => "North Carolina",
+            "postal_code" => "56957-3212",
+            "country" => "US",
+            "phone" => "1-514-133-0708",
+            "email" => null
+        ];
+
+        
+        $orderCreate = new Order($createPayload);
+        $responseCreateOrder = $this->orderHandler->createOrder($orderCreate);
+        $this->assertEquals($orderNumber, $responseCreateOrder->order->getOrderNumber());
+        
+        $updatePayload = [
+            "id" => $responseCreateOrder->order->getId(),
+            "ship_name" => "TEST William Adama UPDATED"
+        ];
+
+        $orderUpdate = new Order($updatePayload);
+        $responseUpdateOrder = $this->orderHandler->updateOrder($orderUpdate);
+        $this->assertEquals($responseCreateOrder->order->getId(), $responseUpdateOrder->order->getId());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldDeleteAnOrder() : void
+    {
+        $orderNumber = date("YmdHis") . rand(0, 100);
+        $createPayload = [
+            "order_number" => $orderNumber,
+            "requested_shipping_service" => "UPS First Class",
+            "ship_name" => "TEST William Adama",
+            "address1" => "3534 Cole Prairie",
+            "address2" => "Suite 703",
+            "city" => "East Justine",
+            "state" => "North Carolina",
+            "postal_code" => "56957-3212",
+            "country" => "US",
+            "phone" => "1-514-133-0708",
+            "email" => null
+        ];
+
+        
+        $orderCreate = new Order($createPayload);
+        $responseCreateOrder = $this->orderHandler->createOrder($orderCreate);
+        $this->assertEquals($orderNumber, $responseCreateOrder->order->getOrderNumber());
+
+        $responseDeleteOrder = $this->orderHandler->deleteOrder($responseCreateOrder->order->getId());
+        $this->assertEquals($responseCreateOrder->order->getId(), $responseDeleteOrder->order->getId());
+        $this->assertEquals('cancelled', $responseDeleteOrder->order->getStatus());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldThrowErrorWhenOrderNumberIsTaken() : void
+    {
+        $orderNumber = date("YmdHis") . rand(0, 100);
+        $payload = [
+            "order_number" => $orderNumber,
+            "requested_shipping_service" => "UPS First Class",
+            "ship_name" => "TEST William Adama",
+            "address1" => "3534 Cole Prairie",
+            "address2" => "Suite 703",
+            "city" => "East Justine",
+            "state" => "North Carolina",
+            "postal_code" => "56957-3212",
+            "country" => "US",
+            "phone" => "1-514-133-0708",
+            "email" => null
+        ];
+
+        $order = new Order($payload);
+        $responseCreateOrder1 = $this->orderHandler->createOrder($order);
+        $this->assertEquals($orderNumber, $responseCreateOrder1->order->getOrderNumber());
+
+        $responseCreateOrder2 = $this->orderHandler->createOrder($order);
+        $this->assertStringContainsString('Order number has already been taken', $responseCreateOrder2->error);
+    }
+}
